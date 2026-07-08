0978042a6e90370cfa5ea31c46a29ebe186e8db8
Cylliun <japaguilherme55@gmail.com>
2026-07-08 13:10:43 -0400

    (feat): Adiciona Dtos de produtos, fornecedores e movimentacoes

---
+++ backend-sistema-controle-estoque/Dtos/Fornecedores/FornecedorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_sistema_controle_estoque.Dtos.Fornecedores;
+
+public record FornecedorDto(
+    string Nome,
+    string Cnpj,
+    string Telefone,
+    string Email
+);
---
+++ backend-sistema-controle-estoque/Dtos/Movimentacao/CriarMovimentacaoDto.cs
@@ -0,0 +1,9 @@
+﻿using backend_sistema_controle_estoque.Models;
+
+namespace backend_sistema_controle_estoque.Dtos.Movimentacao;
+
+public record CriarMovimentacaoDto(
+    int ProdutoId,
+    TipoMovimentacao TipoMovimentacao,
+    int Quantidade
+    );

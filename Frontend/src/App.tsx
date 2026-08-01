109f7c8dc02a01f84718586aeacb96e80bc673ef
Cylliun <japaguilherme55@gmail.com>
2026-08-01 00:05:11 -0400

    chore: dependencias do shadcn + configuracao de rotas do projeto + organizacao de pastas da pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { AppRoutes } from "@/routes/AppRoutes";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Sistema de Controle de Estoque
-      </h1>
-      <Button>Entrar</Button>
-    </div>
+    <AppRoutes />
   );
 }
 
---
+++ Frontend/src/pages/Fornecedores/index.tsx
@@ -0,0 +1,7 @@
+function Fornecedores() {
+    return(
+        <h1>Fornecedores</h1>
+    )
+}
+
+export default Fornecedores
